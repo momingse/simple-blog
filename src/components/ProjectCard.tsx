d463cf854816f07d44491f68cf6c8eb91d67212c
momingse <da1710080850@gmail.com>
2024-03-01 11:54:49 +0800

    feat: add project page

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { ProjectInfo } from "../pages/Project";
+import { Link } from "react-router-dom";
+
+type ProjectCardProps = {
+  projectInfo: ProjectInfo;
+};
+
+const ProjectCard: FC<ProjectCardProps> = ({ projectInfo }) => {
+  const { html_url, name, description, tags, image } = projectInfo;
+
+  return (
+    <div>
+      <Link
+        to={html_url}
+        className="block m-auto max-w-[666px] py-2 px-6 cursor-pointer bg-[#fafafa] rounded-xl text-zinc-500 hover:text-zinc-800 shadow-lg hover:border border-gray-200 md:flex"
+      >
+        <div className="mb-3 md:p-5">
+          <img src={`/assets/${image}`} />
+        </div>
+        <div className="md:p-5">
+          <div className="text-xl md:text-2xl pb-2">{name}</div>
+          <div className="pb-1">{description}</div>
+          <span className="text-sm text-zinc-400 sm:inline-flex flex gap-2 overflow-hidden">
+            {tags?.map((tag) => <span key={tag}>{tag}</span>)}
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ProjectCard;
