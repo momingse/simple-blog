9f58084835938e0076b324e9220e0bb4f9b90e7e
momingse <da1710080850@gmail.com>
2024-04-29 23:11:45 +0800

    chore: test build setting

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-export default app;
