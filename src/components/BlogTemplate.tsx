d8509b24cfdc8a16fba21eb6656f3c613895be28
momingse <da1710080850@gmail.com>
2024-04-29 15:17:51 +0800

    fix: fix formatting in code block

--- src/components/BlogTemplate.tsx
+++ src/components/BlogTemplate.tsx
@@ -1,26 +1,26 @@
-import hljs from "highlight.js";
 import { FC, useEffect, useMemo } from "react";
 import Anchor from "./Anchor";
+import Prism from "prismjs";
 
 type BlogTemplateProps = {
   html: string;
   headingId: {
     level: 2 | 3;
     id: string;
-  }[]
+  }[];
 };
 
 const BlogTemplate: FC<BlogTemplateProps> = ({ html, headingId }) => {
   useEffect(() => {
-    hljs.highlightAll();
-  });
+    Prism.highlightAll();
+  }, []);
 
   const items = useMemo(() => {
     return headingId.map(({ id, level }) => {
       const title = decodeURI(id.replace(/---\d+$/, ""));
       return {
         id,
-        title, 
+        title,
         level,
       };
     });
@@ -28,14 +28,14 @@ const BlogTemplate: FC<BlogTemplateProps> = ({ html, headingId }) => {
 
   return (
     <div className="w-full">
-      <div className="m-auto lg:flex justify-center">
-        <div className="hidden lg:block mr-2 w-40" />
+      <div className="m-auto md:flex justify-center">
+        <div className="hidden lg:block mr-2 w-44" />
         <div
           id="blog"
           className="max-w-[666px]"
           dangerouslySetInnerHTML={{ __html: html }}
         />
-        <div className="hidden lg:block ml-2 w-40 border-zinc-50">
+        <div className="hidden lg:block ml-2 w-44 border-zinc-50">
           <Anchor
             items={items}
             className="sticky top-10 text-zinc-600 text-xs overflow-hidden whitespace-nowrap text-ellipsis"
