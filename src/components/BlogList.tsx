e2ec2b2e470ced2796bb58efc4d82ab52e5787cb
momingse <da1710080850@gmail.com>
2025-05-14 17:25:04 +0800

    feat: sort blog according to date

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,34 +1,42 @@
-import BlogCard from './BlogCard';
-import FadeInWrapper from '../components/FadeInWrapper';
-import { FC } from 'react';
+import BlogCard from "./BlogCard";
+import FadeInWrapper from "../components/FadeInWrapper";
+import { FC } from "react";
 
 type BlogInfo = {
-	blogsInfo: any[];
+  blogsInfo: any[];
 };
 
 const BlogList: FC<BlogInfo> = ({ blogsInfo }) => {
-	return (
-		<FadeInWrapper
-			key={'blog'}
-			className='flex flex-col gap-4 w-full justify-center self-center pt-3'
-			// once={true}
-			dependencies={[blogsInfo]}
-			delay={0.05}
-			duration={0.5}
-		>
-			{blogsInfo.map(({ date, topics, name }) => {
-				return (
-					<BlogCard
-						date={date}
-						topics={topics}
-						name={name}
-						key={name}
-						to={`/blog/${name}`}
-					/>
-				);
-			})}
-		</FadeInWrapper>
-	);
+  const sortedBlogsInfo = [...blogsInfo].sort((a, b) => {
+    const [dayA, monthA, yearA] = a.date.split("/").map(Number);
+    const [dayB, monthB, yearB] = b.date.split("/").map(Number);
+
+    if (yearA !== yearB) return yearB - yearA;
+    if (monthA !== monthB) return monthB - monthA;
+    return dayB - dayA;
+  });
+  return (
+    <FadeInWrapper
+      key={"blog"}
+      className="flex flex-col gap-4 w-full justify-center self-center pt-3"
+      // once={true}
+      dependencies={[blogsInfo]}
+      delay={0.05}
+      duration={0.5}
+    >
+      {sortedBlogsInfo.map(({ date, topics, name }) => {
+        return (
+          <BlogCard
+            date={date}
+            topics={topics}
+            name={name}
+            key={name}
+            to={`/blog/${name}`}
+          />
+        );
+      })}
+    </FadeInWrapper>
+  );
 };
 
 export default BlogList;
