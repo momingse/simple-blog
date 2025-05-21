ddd596e2840c4142947aa76d34426e0334f93c52
momingse <da1710080850@gmail.com>
2025-05-21 12:38:34 +0800

    feat: particles effect on background

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import AppHeader from "./AppHeader";
 import ReturnToTopButton from "./ReturnToTopButton";
+import Particles from "./Particles";
 
 type AppLayoutProps = {
   children: ReactNode;
@@ -9,6 +10,7 @@ type AppLayoutProps = {
 const AppLayout: FC<AppLayoutProps> = ({ children }) => {
   return (
     <div className="relative">
+      <Particles />
       <AppHeader />
       <div className="py-3 px-6 md:py-9">{children}</div>
       <ReturnToTopButton className="fixed bottom-6 right-6" />
